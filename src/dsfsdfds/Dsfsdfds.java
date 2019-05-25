9ca7df9d63b4f8ab9bdc1c13d84810d6203db6e8
scamposr020 <scamposr020@ulacit.ed.cr>
2019-05-24 21:06:17 -0600

    primer repositorio

---
+++ src/dsfsdfds/Dsfsdfds.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsfsdfds;
+
+/**
+ *
+ * @author cr270
+ */
+public class Dsfsdfds {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola weros");
+    }
+    
+}
